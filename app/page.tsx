42449511d84f2f5ed22e0add133590fed01b1a2c
mostafa-be <151264890+mostafa-be@users.noreply.github.com>
2024-07-22 23:19:45 +0100

    The about me component has been completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -9,7 +10,7 @@ export default function Home() {
     <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
       <div className="max-w-7xl w-full ">
         <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+          navItems={navItems}
         />
         <Hero />
         <Grid />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 function Hero() {
   return (
-    <div className="pb-20 pt-36">
+    <div className="py-20 ">
       <div className="">
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -22,7 +22,7 @@ function Hero() {
       <div className="h-screen  w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
-      <div className="flex items-center justify-center relative my-20">
+      <div className="flex items-center justify-center relative ">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic web magic whit Next.js

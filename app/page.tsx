dfb373dc0e9f8d0aa783f054748ed26d0002b3a3
mostafa-be <151264890+mostafa-be@users.noreply.github.com>
2024-07-25 00:24:45 +0100

    there is problem in component approach and footer component is finished. The problem will be solved later

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
 
-
 export default function Home() {
   return (
     <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
       <div className="max-w-7xl w-full ">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
-        <Experience/>
+        <Experience />
+        {/*<Approach />*/}
+        <Footer/>
       </div>
     </main>
   );
